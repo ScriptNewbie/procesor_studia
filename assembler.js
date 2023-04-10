c36ac3e9fbe08b7c3844705a0e9ae94a0311b64a
Marcin Konopka <scriptnewbie0@gmail.com>
2023-04-10 22:15:04 +0200

    Another assembler commit

--- assembler.js
+++ assembler.js
@@ -5,9 +5,20 @@ const generatePayload = (value) => {
     return payload = String("0").repeat(16 - binaryValue.length) + binaryValue
 }
 
+const getLabelAddress = (name, labels) => {
+    for (let i = 0; i < labels.length; i++) {
+      if (labels[i].name === name) {
+        const { line } = labels[i];
+        return line;
+      }
+    }
+    return -1;
+  };
+
 try {
+  let ok = true;
   let data = fs.readFileSync('source.as', 'utf8');
-  data = data.replace(/[\r\t]+/g, ''); //zmienic
+  data = data.replace(/[\r\t]+/g, ' ');
   const lines = data.split('\n');
   let expressions = [];
   lines.forEach(line => {
@@ -22,26 +33,21 @@ try {
   const labels = [];
   expressions.forEach((expression, index) => {
     if(expression[0][expression[0].length-1] == ":") {
-        expression[0] = expression[0].slice(0, -1)
-        labels.push({name: expression[0], line: index})
+        if(isNaN(parseInt(expression[0]))){
+            expression[0] = expression[0].slice(0, -1)
+            labels.push({name: expression[0], line: index})
+        } else {
+            console.log(index + ": Syntax error: Label cannot start with number")
+            ok = false;
+        }
         expression.shift();
     }
   })
 
-  console.log(expressions)
-
   const machineCode = [];
-  expressions.forEach((expression) => {
+  expressions.forEach((expression, index) => {
     const [operation, firstOperand, secondOperand] = expression
-    if(operation === "add" || operation === "sub") {
-        if(typeof firstOperand !== 'undefined') {
-            const address = parseInt(firstOperand.substr(1));
-            if(firstOperand.startsWith("r") && !isNaN(address)) {
-                machineCode.push((operation === "add" ? "00000" : "00001") + generatePayload(address))
-            } else console.log("Syntax error: Add requires register as argument");
-        } else console.log("Syntax error: Add requires operand");
-    }
-    else if(operation === "mov") {
+    if(operation === "mov") {
         if(typeof firstOperand !== 'undefined' && typeof secondOperand !== 'undefined') {
             if(firstOperand == "a") {
                 if(!isNaN(secondOperand))
@@ -53,18 +59,42 @@ try {
                 } else if(secondOperand.startsWith("@"))
                 {
                     machineCode.push("11110" + generatePayload(parseInt(secondOperand.substr(1))))
-                } else console.log("Syntax error: Wrong second operand!");
-            } else {
+                } else {
+                    console.log(index+ ": Syntax error: Wrong second operand!");
+                    ok = false;
+                }
+            } else if(secondOperand === "a") {
                 if(firstOperand.startsWith("r"))
                 {
                     machineCode.push("11001" + generatePayload(parseInt(firstOperand.substr(1))))
                 } else if(firstOperand.startsWith("@"))
                 {
                     machineCode.push("11010" + generatePayload(parseInt(firstOperand.substr(1))))
-                } else console.log("Syntax error: Wrong second operand!");
+                } else {
+                    console.log(index+ ": Syntax error: Wrong second operand!");
+                    ok = false;
+                }
 
+            } else {
+                console.log(index+ ": Syntax error: One operand must be acumulator")
             }
-        } else console.log("Syntax error: Add requires two operands");
+        } else {
+            console.log(index+ ": Syntax error: Add requires two operands");
+            ok = false;
+        }
+    } else if(operation === "add" || operation === "sub") {
+        if(typeof firstOperand !== 'undefined') {
+            const address = parseInt(firstOperand.substr(1));
+            if(firstOperand.startsWith("r") && !isNaN(address)) {
+                machineCode.push((operation === "add" ? "00000" : "00100") + generatePayload(address))
+            } else {
+                console.log(index+ ": Syntax error: Add requires register as argument");
+                ok = false;
+            }
+        } else {
+            console.log(index+ ": Syntax error: Add requires operand");
+            ok = false;
+        }
     } else if(operation === "inc" || operation == "dec"){
         if(typeof firstOperand === 'undefined' && typeof secondOperand === 'undefined') {
             machineCode.push((operation === "inc" ? "00001" : "00101") + generatePayload(1))
@@ -73,19 +103,72 @@ try {
             if(!isNaN(value)){
                 machineCode.push((operation === "inc" ? "00001" : "00101") + generatePayload(value))
             } else {
-                console.log("Syntax error: Operand is wrong!")
+                console.log(index+ ": Syntax error: Operand is wrong!")
+                ok = false;
             } 
         } else {
-            console.log("Syntax error: Wrong operands!")
+            console.log(index+ ": Syntax error: Wrong operands!")
+            ok = false;
         }
-    }
-    else if(operation === "ret") {
+    } else if(operation === "and"){
+        if(typeof firstOperand !== 'undefined') {
+            const address = parseInt(firstOperand.substr(1));
+            if(firstOperand.startsWith("r") && !isNaN(address)) {
+                machineCode.push( "01000" + generatePayload(address))
+            } else {
+                console.log(index+ ": Syntax error: AND requires register as argument");
+                ok = false;
+            }
+        } else {
+            console.log(index+ ": Syntax error: AND requires operand");
+            ok = false;
+        }
+    } else if(operation === "or"){
+        if(typeof firstOperand !== 'undefined') {
+            const address = parseInt(firstOperand.substr(1));
+            if(firstOperand.startsWith("r") && !isNaN(address)) {
+                machineCode.push( "01100" + generatePayload(address))
+            } else {
+                console.log(index+ ": Syntax error: OR requires register as argument");
+                ok = false;
+            }
+        } else {
+            console.log(index+ ": Syntax error: OR requires operand");
+            ok = false;
+        }
+    } else if(operation === "xor"){
+        if(typeof firstOperand !== 'undefined') {
+            const address = parseInt(firstOperand.substr(1));
+            if(firstOperand.startsWith("r") && !isNaN(address)) {
+                machineCode.push( "10000" + generatePayload(address))
+            } else {
+                console.log(index+ ": Syntax error: XOR requires register as argument");
+                ok = false;
+            }
+        } else {
+            console.log(index+ ": Syntax error: XOR requires operand");
+            ok = false;
+        }
+    } else if(operation === "not") {
         machineCode.push("100010000000000000000")
-    } else {
+    } else if(operation === "ret") {
+        machineCode.push("100010000000000000000")
+    } else if(operation ==="nop"){
         machineCode.push("110110000000000000000")
+    } else {
+        console.log(index+ ": Syntax error: No such mnemonic")
+        ok = false;
     }
   })
-  console.log(machineCode)
+
+  if(ok){
+    console.log(expressions)
+    console.log(labels)
+    console.log(machineCode)
+    
+  }
+  
+
 } catch (err) {
   console.error(err);
 }
--- source.as
+++ source.as
@@ -1,10 +1,14 @@
-loop:	mov a, 4 //komentarz
-		mov r0, a
-		mov a, r0
-		//komentarz
-2loop:		ADD r31
-		sub r11
-		nop
+loop:	add r2
 		inc
+		sub r1
 		dec
-		inc 3
+loop2:	and r1
+		or r1
+		xor r1
+		mov r1, a
+		mov @2, a
+		nop
+		mov a, r1
+		mov a, 5
+		mov a, @3
+		ret
